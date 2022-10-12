d60b33d7494b3e74f9608b1e3df995a3d836eea8
Even Koshliak <evenkoshliak@o-s-i.org>
2022-10-12 19:04:34 +0300

    HTML/CSS - remove useless code, add game cards placeholders

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,3 +1,32 @@
 import '../scss/styles.scss';
 
-console.log('hello!')
+interface IGameCards {
+    classList: {
+        [key: string]: string;
+    };
+}
+
+class GameCards {
+    // protected options: IGameCards = {
+    //     classList: {
+    //         cardsItem: 'cards__item'
+    //     }
+    // }
+    private elements: HTMLElement[] = [];
+    private readonly selector: string
+
+    public constructor(selector: string) {
+        this.selector = selector;
+        this.elements = Array.from(document.querySelectorAll<HTMLElement>(this.selector));
+
+        this.init();
+    }
+
+    private init(): void {
+        this.elements.forEach(item => {
+            console.log(item);
+        })
+    }
+}
+
+new GameCards('.cards__item');
