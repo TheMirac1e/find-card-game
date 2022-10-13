21347a031b6dadfee9a0058d1b5734d49b7221f1
Even Koshliak <evenkoshliak@o-s-i.org>
2022-10-13 18:42:39 +0300

    HTML/CSS/JS - add game logic

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,19 +1,21 @@
 import '../scss/styles.scss';
 
 interface IGameCards {
-    classList: {
-        [key: string]: string;
-    };
+    cardsNumber: number;
 }
 
 class GameCards {
-    // protected options: IGameCards = {
-    //     classList: {
-    //         cardsItem: 'cards__item'
-    //     }
-    // }
-    private elements: HTMLElement[] = [];
-    private readonly selector: string
+    protected options: IGameCards = {
+        cardsNumber: 12,
+    }
+
+    private readonly elements: HTMLElement[] = [];
+    private readonly selector: string;
+    private firstCard: HTMLElement | null = null;
+    private secondCard: HTMLElement | null = null;
+
+    private flippedCard = false;
+    private lockCards = false;
 
     public constructor(selector: string) {
         this.selector = selector;
@@ -23,15 +25,69 @@ class GameCards {
     }
 
     private init(): void {
-        this.toggleClass()
+        this.addClickEvent();
+        this.randomizeItems();
     }
 
-    private toggleClass(): void {
-       this.elements.forEach(item => {
-           item.addEventListener('click', () => {
-               item.classList.add('is-active');
-           })
-       })
+    private addClickEvent(): void {
+        this.elements.forEach(item => {
+            item.addEventListener('click', () => {
+                this.flipCard(item);
+            })
+        })
+    }
+
+    private flipCard(card: HTMLElement): any {
+        if (this.lockCards) return;
+        if (card === this.firstCard) return;
+        card.classList.add('is-active');
+
+        if (!this.flippedCard) {
+            this.firstCard = card;
+            this.flippedCard = true;
+            return;
+        }
+
+        this.secondCard = card;
+        this.lockCards = true;
+        this.checkCards();
+    }
+
+    private checkCards(): void {
+        const isSame = this.firstCard?.dataset.item === this.secondCard?.dataset.item;
+
+        isSame ? this.removeClickEvent() : this.unflipCard();
+    }
+
+    private resetActiveItems(): void {
+        this.firstCard = null;
+        this.secondCard = null;
+
+        this.flippedCard = false;
+        this.lockCards = false;
+    }
+
+    private removeClickEvent(): void {
+        this.secondCard?.removeEventListener('click', this.flipCard(this.secondCard));
+        this.firstCard?.removeEventListener('click', this.flipCard(this.firstCard));
+
+        this.resetActiveItems();
+    }
+
+    private unflipCard(): void {
+        setTimeout(() => {
+            this.firstCard?.classList.remove('is-active');
+            this.secondCard?.classList.remove('is-active');
+
+            this.resetActiveItems();
+        }, 1000);
+    }
+
+    private randomizeItems(): void {
+        this.elements.forEach(card => {
+            const orderNumber = Math.floor(Math.random() * this.options.cardsNumber);
+            card.style.order = orderNumber.toString();
+        })
     }
 }
 
